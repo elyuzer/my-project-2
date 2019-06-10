3cbd44dff6c37ab5061b15016a3634f4080cbc6b
Dickens Maroko <marokod@outlook.com>
2019-06-10 14:09:36 +0300

    Fourth commit

--- hello-world.php
+++ hello-world.php
@@ -5,9 +5,13 @@ $a = 5;
 $b = 3;
 
 echo 'The sum of '.$a.' and '.$b.' is '.add($a,$b);
+echo '<br /> Difference between '.$a.' and '.$b.' is '.sub($a,$b);
 
 function add($a, $b){
   return $a + $b;
 }
 
+function sub($a, $b){
+  return $a - $b;
+}
 ?>
