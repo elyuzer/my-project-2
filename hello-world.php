d7419eaac537350bde05b69940310014025325aa
Dickens Maroko <marokod@outlook.com>
2019-06-11 08:45:36 +0300

    Fift commit

--- hello-world.php
+++ hello-world.php
@@ -4,8 +4,9 @@ echo "Hello World!";
 $a = 5;
 $b = 3;
 
-echo 'The sum of '.$a.' and '.$b.' is '.add($a,$b);
-echo '<br /> Difference between '.$a.' and '.$b.' is '.sub($a,$b);
+echo '<br />The sum of '.$a.' and '.$b.' is '.add($a,$b);
+echo '<br />Difference between '.$a.' and '.$b.' is '.sub($a,$b);
+echo '<br />The product of '.$a.' and '.$b.' is '.multiply($a, $b);
 
 function add($a, $b){
   return $a + $b;
@@ -13,5 +14,9 @@ function add($a, $b){
 
 function sub($a, $b){
   return $a - $b;
+}
+
+function multiply(){
+  return $a * $b;
 }
 ?>
